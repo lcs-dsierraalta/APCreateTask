e248fb6188ff597e519767e9fafcf29858736947
Diego Sierraalta <dsierraalta@lcs.on.ca>
2022-03-25 12:17:18 -0400

    Initial Commit

---
+++ APCreateTask/APCreateTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  APCreateTaskApp.swift
+//  APCreateTask
+//
+//  Created by Diego Sierraalta on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct APCreateTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
