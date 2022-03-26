91f77a0f19699fd996fe062f3b55c34ced1e0b49
Diego Sierraalta <dsierraalta@lcs.on.ca>
2022-03-26 10:34:20 -0400

    Added a title to the app, a structure that defines each person's birthday, and some test data to use for later

---
+++ APCreateTask/Models/BirthdayPerson.swift
@@ -0,0 +1,19 @@
+//
+//  BirthdayPerson.swift
+//  APCreateTask
+//
+//  Created by Diego Sierraalta on 2022-03-26.
+//
+
+import Foundation
+
+struct Person {
+    var name: String
+    var date = Date()
+}
+
+let testData = [
+    Person(name: "Kia", date: Date()),
+    Person(name: "Moritz", date: Date()),
+    Person(name: "Diego Sierraalta", date: Date())
+]
--- APCreateTask/Views/ContentView.swift
+++ APCreateTask/Views/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Birthday App")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.leading)
+            
+            List {
+                
+            }
+        }
     }
 }
 
